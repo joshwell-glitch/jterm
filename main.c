e1d4076ec27aebb19322b6d0fc47b4c7c6d9d18d
joshwell-glitch <joshwellcabalhin@gmail.com>
2026-09-25 07:26:48 +0800

    setup

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
+  int i = 0;
+  int *number = malloc(i * sizeof(int));
+
+  if(number == NULL)
+  {
+    printf("Memory Allocation Failed!");
+    return 1;
+  }
+
+  free(number);
+
   return 0;
 }
